fa5bfc575821617481603138f7e8265529925dc3
NuggetBox <nilsbenjaminek@gmail.com>
2022-11-08 17:53:54 +0100

    streaming

--- Source/GraphicsEngine/Rendering/GraphicsEngine.h
+++ Source/GraphicsEngine/Rendering/GraphicsEngine.h
@@ -66,8 +66,13 @@ private:
 	void ResetStates();
 	void ResetViewport();
 
+	static void LoadModel(std::filesystem::path path, std::shared_ptr<Model> model);
+	static inline std::atomic_bool myFirstThreadStarted = false;
+	static inline std::atomic_bool mySecondThreadStarted = false;
+	static inline std::atomic_bool myThirdThreadStarted = false;
+
 	void CameraControls(std::shared_ptr<Camera> aCamera);
-	float myCameraSpeed = 100.0f;
+	float myCameraSpeed = 250.0f;
 
 	std::string RenderModeToString(RenderMode aRenderMode);
 
